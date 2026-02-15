4e63ad654fe4561d7d661ca382549b11061dae58
Antony Diego <artbsb1@gmail.com>
2026-02-15 14:19:00 -0300

    style(body): altera a cor padrão do body

--- frontend/src/app/components/section-header/section-header.ts
+++ frontend/src/app/components/section-header/section-header.ts
@@ -1,11 +1,13 @@
+import { NgClass } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-section-header',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './section-header.html',
   styleUrl: './section-header.css',
 })
 export class SectionHeader {
   readonly titulo = input.required<string>();
+  readonly variante = input<'padrao' | 'branco'>('padrao');
 }
